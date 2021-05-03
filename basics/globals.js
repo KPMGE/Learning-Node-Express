a68a621f0f24a6356c28cc05d0b725a47c6929e9
KPMGE <jesuskevin254@gmail.com>
2021-05-03 14:19:54 -0300

    Adding sample with globals

---
+++ basics/globals.js
@@ -0,0 +1,12 @@
+// The most common globals in Node 
+// Author: Kevin Carvalho de jesus
+
+// __dirname  - path to current directory
+// __filename - file name
+// require    - function to use modules (CommonJS)
+// module     - info about current module (file)
+// process    - info about env where the program is being executed
+
+console.log('__filename: ', __filename);
+console.log('__dirname: ', __dirname);
+
